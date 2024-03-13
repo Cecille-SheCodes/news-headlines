a2b36ac70baef8373f473e9ef943ba1c6c79e2df
Cecille-SheCodes <137907754+Cecille-SheCodes@users.noreply.github.com>
2024-03-13 11:14:19 +0100

    added the submit function in js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-alert("Hi");
+function generateNews(event) {
+  event.preventDefault();
+
+  new Typewriter(".searchedWord", {
+    strings: "!Latest News Update About The Searched Word!",
+    autoStart: true,
+    cursor: "",
+    delay: 15,
+  });
+  
+  let newsBox = document.querySelector(".news-headlines");
+ 
+ 
+  newsBox.innerHTML =
+    "<h3> Title of News1</h3>" +
+    "<p> Text about the news</p>" +
+    "<hr>" +
+    "<h3> Title of News2</h3>" +
+    "<p> Text about the news</p>" +
+    "<hr>" +
+    "<h3> Title of News3</h3>" +
+    "<p> Text about the news</p>" +
+    "<hr>";
+}
+
+let searchForm = document.querySelector(".search-bar");
+searchForm.addEventListener("submit", generateNews);
